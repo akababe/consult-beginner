4832fc00987c7edd9f744a9337c98124664427d7
akababe <akaarinda@gmail.com>
2026-05-18 23:05:17 +0700

    re route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/consult-beginner">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
